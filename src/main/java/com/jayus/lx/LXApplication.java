d29ea5e6ea6b4c570783d740476b6fbe38e38056
JayusHuang <1372309971@qq.com>
2022-10-19 09:33:52 +0800

    init:新建springboot项目

---
+++ src/main/java/com/jayus/lx/LXApplication.java
@@ -0,0 +1,17 @@
+package com.jayus.lx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ Description
+ * @ Author jayus
+ * @ Date 2022/10/19 9:25
+ */
+@SpringBootApplication
+public class LXApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LXApplication.class,args);
+    }
+}
+
